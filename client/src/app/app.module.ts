0e2933484db8dcc75a5e66a6553a594b0b9db1cb
Thong Dinh Duc <thong.dinhduc@eightcap.com>
2023-11-03 16:19:11 +0700

    Add components.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { BannerComponent } from './banner/banner.component';
+import { HomeProductSectionComponent } from './home-product-section/home-product-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    MenuComponent,
+    HomeComponent,
+    BannerComponent,
+    HomeProductSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/home-product-section/home-product-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-product-section',
+  templateUrl: './home-product-section.component.html',
+  styleUrls: ['./home-product-section.component.css']
+})
+export class HomeProductSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
