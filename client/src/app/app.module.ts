36a6a39d99067e15a55c69fc562d4b0196af8cff
Thong Dinh Duc <thong.dinhduc@eightcap.com>
2023-11-03 19:27:37 +0700

    Add components.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { BannerComponent } from './banner/banner.component';
 import { HomeProductSectionComponent } from './home-product-section/home-product-section.component';
+import { ProductCategoryMenuComponent } from './product-category-menu/product-category-menu.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { HomeProductSectionComponent } from './home-product-section/home-product
     MenuComponent,
     HomeComponent,
     BannerComponent,
-    HomeProductSectionComponent
+    HomeProductSectionComponent,
+    ProductCategoryMenuComponent
   ],
   imports: [
     BrowserModule,
